a12d195cc0ab8f32063e68f0d8ff87db700a5548
AgnesePinto <agnese.pinto01@universitadipavia.it>
2026-03-23 12:10:48 +0100

    Refactoring GPData

--- GPModel/GPRecord/GPData.cs
+++ GPModel/GPRecord/GPData.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Gaze_Point.GPModel.GPRecord
+﻿namespace Gaze_Point.GPModel.GPRecord
 {
 
     /// <summary>
