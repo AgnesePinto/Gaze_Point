beb0890421daeb1de7a4cfce17d9dbc31269ae26
AgnesePinto <agnese.pinto01@universitadipavia.it>
2026-03-29 16:41:57 +0200

    Refactoring GPCursor

--- GPModel/GPCursor/GPCursor.cs
+++ GPModel/GPCursor/GPCursor.cs
@@ -4,7 +4,8 @@ namespace Gaze_Point.GPModel.GPCursor
 {
 
     /// <summary>
-    /// 
+    /// Represents the visual gaze cursor state for UI binding.
+    /// Implements INotifyPropertyChanged to enable real-time position updates on the screen.
     /// </summary>
     /// <author>Agnese Pinto</author>
     public class GPCursor : INotifyPropertyChanged
@@ -12,21 +13,30 @@ namespace Gaze_Point.GPModel.GPCursor
         private double _x;
         private double _y;
 
-        // Posizione X calcolata per l'interfaccia (Pixel Logici)
+
+        /// <summary>
+        /// Gets or set the X-coordinate position in logical pixels (DPI-independent).
+        /// </summary>
         public double X
         {
             get => _x;
             set { _x = value; OnPropertyChanged(nameof(X)); }
         }
 
-        // Posizione Y calcolata per l'interfaccia (Pixel Logici)
+
+        /// <summary>
+        /// Gets or set the Y-coordinate position in logical pixels (DPI-independent).
+        /// </summary>
         public double Y
         {
             get => _y;
             set { _y = value; OnPropertyChanged(nameof(Y)); }
         }
 
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+
         protected void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
