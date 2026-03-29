b59658a4e6b3b5dad9e88f346396e279f41f628e
AgnesePinto <agnese.pinto01@universitadipavia.it>
2026-03-29 17:44:52 +0200

    Creazione GPInteractiveElements

---
+++ GPModel/GPInteraction/GPInteractiveElements.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Gaze_Point.GPModel.GPInteraction
+{
+
+    /// <summary>
+    /// Global registry for identifying UI element types that the gaze system should consider inactive.
+    /// </summary>
+    /// <author>Agnese Pinto</author>
+     
+
+    public static class GPInteractiveElements
+    {
+        public static readonly HashSet<Type> InteractiveTypes = new HashSet<Type>
+            {
+                typeof(Button),
+                typeof(TextBox),
+                typeof(CheckBox),
+                typeof(RadioButton),
+                typeof(ComboBox),
+                typeof(ComboBoxItem),
+                typeof(MenuItem)
+            };
+
+    }
+}
